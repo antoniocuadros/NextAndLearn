69568dde47aa1c8a55695d926c8d661af7b3fb02
antoniocuadros <antculap@gmail.com>
2021-05-02 17:12:36 +0200

    Closes #5, añadida navegabilidad entre fragmentos

--- src/app/src/main/java/com/example/nextandlearn/MainActivity.kt
+++ src/app/src/main/java/com/example/nextandlearn/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.nextandlearn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Establecemos el controlador del menu inferior
+        estableceControlNavegacion()
+
+        
+    }
+
+
+    private fun estableceControlNavegacion(){
+        val menu_inferior = findViewById<BottomNavigationView>(R.id.menu_inferior)
+        val controlador_navegacion = findNavController(R.id.fragment2)
+        menu_inferior.setupWithNavController(controlador_navegacion)
     }
 }
