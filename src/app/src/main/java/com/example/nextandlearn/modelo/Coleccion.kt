c944b29ff0f8501952df2ba42a5adf4a3cbaae22
antoniocuadros <antculap@gmail.com>
2021-05-02 18:24:08 +0200

    Avanza #6, añadidos comentarios a la clase colección

--- src/app/src/main/java/com/example/nextandlearn/modelo/Coleccion.kt
+++ src/app/src/main/java/com/example/nextandlearn/modelo/Coleccion.kt
@@ -3,6 +3,14 @@ package com.example.nextandlearn.modelo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+/*
+    Esta clase representa una colección de vocabulario que podemos encontrar en nuestra aplicación.
+    Los atributos de la clase Vocabulario son los siguientes:
+        - nombre_coleccion: Nombre de la colección, de tipo String.
+        - nivel: Nivel recomendado para aprender la colección, de tipo String.
+        - identificador: Identificador de la colección, de tipo String.
+        - imagen: Imagen asociada a la colección, de tipo String.
+ */
 @Entity(tableName = "Colecciones")
 data class Coleccion(@PrimaryKey val nombre_coleccion:String,
                      val nivel:String,
