7e9fb7577db3a6b7594605ac5b95b5bbe421cd7d
antoniocuadros <antculap@gmail.com>
2021-05-03 17:17:22 +0200

    Avanza #9, añadido palabra_item como layout y adicionalmente añadido nuevo fragmento que contendrá el sliderView

---
+++ src/app/src/main/java/com/example/nextandlearn/fragmentos/fragmentoCartasPalabras.kt
@@ -0,0 +1,28 @@
+package com.example.nextandlearn.fragmentos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.nextandlearn.R
+
+
+class fragmentoCartasPalabras : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragmento_cartas_palabras, container, false)
+
+        return view
+    }
+    
+}
