dd9e957774937eef9ac4bd0ed29c60faf427ac68
antoniocuadros <antculap@gmail.com>
2021-05-04 16:26:54 +0200

    Avanza #12, añadido fragmento

---
+++ src/app/src/main/java/com/example/nextandlearn/fragmentos/fragmentoMenuTestsVocabulario.kt
@@ -0,0 +1,25 @@
+package com.example.nextandlearn.fragmentos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.nextandlearn.R
+
+class fragmentoMenuTestsVocabulario : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragmento_menu_tests_vocabulario, container, false)
+
+        return view
+    }
+}
