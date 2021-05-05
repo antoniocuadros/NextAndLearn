69d66510472fce863e882b70f47dad7e3b7191f5
antoniocuadros <antculap@gmail.com>
2021-05-05 11:25:25 +0200

    Avanza #13, tests funcionando, falta redireccion final y cambio en base de datos al completar

--- src/app/src/main/java/com/example/nextandlearn/fragmentos/fragmentoTests.kt
+++ src/app/src/main/java/com/example/nextandlearn/fragmentos/fragmentoTests.kt
@@ -22,6 +22,9 @@ class fragmentoTests : Fragment() {
     private var pregunta = 0
     private var numero_preguntas = 0
     private lateinit var opcion_elegida:Palabra
+    private var aciertos = 0
+    private var fallos = 0
+    private lateinit var opciones: MutableList<Palabra>
 
     private lateinit var enunciado:TextView
     private lateinit var imagen_opcion_1:ImageView
@@ -37,6 +40,10 @@ class fragmentoTests : Fragment() {
     private lateinit var opcion_2:LinearLayout
     private lateinit var opcion_3:LinearLayout
     private lateinit var opcion_4:LinearLayout
+    private lateinit var layout_test:LinearLayout
+    private lateinit var layout_resultado:LinearLayout
+    private lateinit var num_aciertos_ly:TextView
+    private lateinit var num_fallos_ly:TextView
 
     private lateinit var boton_siguiente_acierto_fallo:Button
     private lateinit var texto_acierto_fallo:TextView
@@ -61,22 +68,25 @@ class fragmentoTests : Fragment() {
         //Obtenemos todas las palabras de la coleccion, se almacenan en la variable de clase
         obtener_palabras()
 
-
         //Obtenemos las opciones
-        var opciones:MutableList<Palabra> = generaOpciones()
+        opciones = generaOpciones()
 
         //Las añadimos a la vista
-        anadeOpcionesVista(opciones)
+        anadeOpcionesVista()
 
+        // Añadimos los listeners de los botones de las opciones
+        anadeListenersOpciones()
 
-        /*
-            Añadimos los listeners de los botones de las opciones
-         */
-        anadeListenersOpciones(opciones)
+        // Gestionamos el comportamiento del botón siguiente
+        gestionaBotonSiguiente()
 
         return view
     }
 
+    /*
+        Este método se encarga de cambiar de color al pulsar sobre cada uno de los elementos,
+        desmarca la opción anterior y marca la nueva.
+     */
     private fun cambiaColorSeleccionada(opcion:Int){
         when(opcion){
             1 -> {
@@ -103,6 +113,98 @@ class fragmentoTests : Fragment() {
                 opcion_3.setBackgroundColor(Color.parseColor("#E0BBE4"))
                 opcion_4.setBackgroundColor(Color.parseColor("#B165D4"))
             }
+            5 -> {
+                opcion_1.setBackgroundColor(Color.parseColor("#E0BBE4"))
+                opcion_2.setBackgroundColor(Color.parseColor("#E0BBE4"))
+                opcion_3.setBackgroundColor(Color.parseColor("#E0BBE4"))
+                opcion_4.setBackgroundColor(Color.parseColor("#E0BBE4"))
+            }
+        }
+    }
+
+    /*
+        Este método se encarga de gestionar el botón de siguiente de la pestaña acierto o fallo
+        Tenemos que formular una nueva pregunta
+     */
+    private fun gestionaSiguienteAciertoFallo(){
+        boton_siguiente_acierto_fallo.setOnClickListener{
+            if(boton_siguiente_acierto_fallo.text == "Finalizar"){ //Hemos terminado, vamos a la pantalla de colecciones
+                Toast.makeText(context, "ububuebuebue", Toast.LENGTH_SHORT).show()
+                
+            }
+            else{ //Generamos las siguientes
+                if(pregunta < numero_preguntas-1){
+                    pregunta++
+                    opciones = generaOpciones()
+                    anadeOpcionesVista()
+
+                    layout_test.visibility = View.VISIBLE
+                    layout_resultado.visibility = View.GONE
+
+                    if(pregunta == numero_preguntas-1){ //Es la última pregunta, mostramos resultados
+                        num_fallos_ly.visibility = View.VISIBLE
+                        num_aciertos_ly.visibility = View.VISIBLE
+                        num_aciertos_ly.text = "Aciertos: " + aciertos
+                        num_fallos_ly.text = "Fallos: " + fallos
+                        boton_siguiente_acierto_fallo.text = "Finalizar"
+
+                    }
+                }
+                else{
+                    boton_siguiente_acierto_fallo.text = "Finalizar"
+                }
+            }
+
+        }
+    }
+
+    /*
+        Este método se encarga de gestionar los aciertos y fallos
+     */
+    private fun gestionaAciertoFallo(acierto:Boolean){
+        layout_test.visibility = View.GONE
+        layout_resultado.visibility = View.VISIBLE
+        if(acierto){
+            texto_acierto_fallo.text = "¡Acierto!"
+            aciertos++
+        }
+        else{
+            texto_acierto_fallo.text = "Fallo"
+            fallos++
+        }
+        if(pregunta == numero_preguntas-1){
+            if(fallos < 2){ //Ha terminado los tests correctamente
+                texto_acierto_fallo.text = "¡Has completado esta colección!"
+                boton_siguiente_acierto_fallo.text = "Finalizar"
+            }
+            else { //No ha terminado los tests por tener 2 o más fallos
+                texto_acierto_fallo.text = "Intente de nuevo el test para terminar la colección"
+                texto_acierto_fallo.textSize = 40.0F
+            }
+        }
+
+
+        gestionaSiguienteAciertoFallo()
+    }
+
+    /*
+    Este método define el listener del botón siguiente de cada test y su comportamiento
+     */
+    private fun gestionaBotonSiguiente(){
+        boton_siguiente_test.setOnClickListener{
+            if(opcion_elegida.ingles == "none"){
+                Toast.makeText(context, "Seleccione primero una opción", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                if(opcion_elegida == vocabulario[pregunta]){
+                    gestionaAciertoFallo(true)
+                }
+                else{
+                    gestionaAciertoFallo(false)
+                }
+                opcion_elegida = Palabra("none","none", "none","none",false)
+                cambiaColorSeleccionada(5)
+            }
         }
     }
 
@@ -110,28 +212,24 @@ class fragmentoTests : Fragment() {
         Este método se encarga de definir los listener de cada una de las cuatro opciones posibles
         y almacena el valor elegido en una variable de la clase para su posterior tratamiento.
      */
-    private fun anadeListenersOpciones(opciones:MutableList<Palabra>){
+    private fun anadeListenersOpciones(){
         opcion_1.setOnClickListener {
             opcion_elegida = opciones[0]
-            Toast.makeText(context, opcion_elegida.toString(), Toast.LENGTH_SHORT).show()
             cambiaColorSeleccionada(1)
         }
 
         opcion_2.setOnClickListener {
             opcion_elegida = opciones[1]
-            Toast.makeText(context, opcion_elegida.toString(), Toast.LENGTH_SHORT).show()
             cambiaColorSeleccionada(2)
         }
 
         opcion_3.setOnClickListener {
             opcion_elegida = opciones[2]
-            Toast.makeText(context, opcion_elegida.toString(), Toast.LENGTH_SHORT).show()
             cambiaColorSeleccionada(3)
         }
 
         opcion_4.setOnClickListener {
             opcion_elegida = opciones[3]
-            Toast.makeText(context, opcion_elegida.toString(), Toast.LENGTH_SHORT).show()
             cambiaColorSeleccionada(4)
         }
 
@@ -149,7 +247,7 @@ class fragmentoTests : Fragment() {
         Este método se encarga de dado un conjunto de opciones añadirlas a la vista para que se muestren
         junto a un enunciado formando una pregunta del test
      */
-    private fun anadeOpcionesVista(opciones:MutableList<Palabra>){
+    private fun anadeOpcionesVista(){
         var identificador_imagen:Int
 
         enunciado.text = "¿Cual de las siguientes opciones es " + vocabulario[pregunta].espanol.capitalize() + " en inglés?"
@@ -245,6 +343,10 @@ class fragmentoTests : Fragment() {
         opcion_2 = view.findViewById(R.id.opcion2)
         opcion_3 = view.findViewById(R.id.opcion3)
         opcion_4 = view.findViewById(R.id.opcion4)
+        layout_test = view.findViewById(R.id.layout_test)
+        layout_resultado = view.findViewById(R.id.layout_resultado)
+        num_aciertos_ly = view.findViewById(R.id.aciertos_ly)
+        num_fallos_ly = view.findViewById(R.id.fallos_ly)
 
         boton_siguiente_acierto_fallo = view.findViewById(R.id.boton_siguiente2)
         texto_acierto_fallo = view.findViewById(R.id.mensaje_resultado)
